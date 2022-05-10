5f536f4b8d587ee0a511f185b26c8515e62919cc
virtuozlo <iv.chud@yandex.ru>
2022-05-10 23:06:57 +0300

    Добавил вывод даты формата (datetime.date)

--- utils/misc/analyze_callback_calendar.py
+++ utils/misc/analyze_callback_calendar.py
@@ -1,14 +1,36 @@
 """
 В перспективе сделать утилиты для вывода разных даты по выбранной дате: str,list,date и пр.
 """
+import datetime
+
+
+def calendar_calldate_list(data):
+    """
+    :param data:Список с действием и датой
+    :return:Дату списком [year,month,day]
+    """
+    return [int(elem) for elem in data.split(';')[1:]]
+
+
+def format_call_from_date(data) -> datetime.date:
+    """
+    :param data: список с действием и датой
+    :param data: меняется на список с числами даты
+    :return: datetime.date
+    """
+    data = calendar_calldate_list(data)
+    calendar_date = {
+        'year': data[0],
+        'month': data[1],
+        'day': data[2],
+    }
+    return datetime.date(year=calendar_date['year'], month=calendar_date['month'], day=calendar_date['day'])
 
 
 def exit_string_date(data):
     """
-
     :param data: Информация с коллбэк календаря
-    :return: Строку для вывода
+    :return: Строку для вывода в чат бота
     """
-    date = data.split(';')[1:]
-    exit_string = '/'.join(date)
-    return f'Выбранная вами дата: {exit_string}'
+    data = format_call_from_date(data)
+    return f'Выбранная вами дата: {data}'
