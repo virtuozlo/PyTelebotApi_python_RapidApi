089bcadf9e19a4e2a5b1a6d41b2abe20cbce5530
virtuozlo <iv.chud@yandex.ru>
2022-05-15 13:59:29 +0300

    Изменил базу данных на класс

--- database/user_db.py
+++ database/user_db.py
@@ -1,36 +1,48 @@
 import sqlite3
 
 
-def set_up_db():
-    """
-    Создание базы данных
-    :return:
-    """
-    con = sqlite3.connect('user.db')
-    with con:
-        con.execute('''
-        CREATE TABLE IF NOT EXISTS User(
-        userId INT PRIMARY KEY ,
-        fname TEXT,
-           lname TEXT,
-           AGE INT NOT NULL )
-        ''')
+class UserDb:
+    def __init__(self, db_file):
+        self.connection = sqlite3.connect(db_file, check_same_thread=False)
+        self.cursor = self.connection.cursor()
+        self.set_up_db()
 
+    def set_up_db(self):
+        """
+        :return: Создание базы данных
+        """
+        with self.connection:
+            return self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS User(
+            userId INT PRIMARY KEY ,
+            fname TEXT,
+            lname TEXT,
+            AGE INT NOT NULL DEFAULT 1 )
+            ''')
 
-def filling_db(data):
-    """
-    Функция записи информации о пользователе из State_handlers.Если пользователь есть, то обновляет данные
-    :param data: State telebot
-    :return:
-    """
-    cur = sqlite3.connect('user.db')
-    with cur:
-        cur.execute('SELECT userId FROM User WHERE userId = (?)', (data['id'],))
-        dat = cur.cursor().fetchall()
-        if len(dat) != 0:
-            cur.execute('UPDATE User SET fname=?,lname=?,AGE=? WHERE userID=?',
-                        (data['name'], data['surname'], data['age'], data['id']))
-        else:
-            cur.execute('''
-            INSERT INTO User(userID,fname,lname,AGE)
-            VALUES (?,?,?,?)''', (data['id'], data['name'], data['surname'], data['age']))
+    def check_user(self, user_id):
+        """
+        :param user_id: идентификатор пользователя
+        :return: Наличие/отсутствие пользователя
+        """
+        with self.connection:
+            result = self.cursor.execute('SELECT `userId` FROM User WHERE `userId` = ?', (user_id,)).fetchall()
+            return bool(len(result))
+
+    def add_user(self, user_id):
+        """
+        :param user_id: Идентификатор пользователя
+        :return: Добавить пользователя  БД
+        """
+        with self.connection:
+            return self.cursor.execute('INSERT INTO User(UserId) VALUES (?)', (user_id,))
+
+    def filling_db(self, data):
+        """
+        Функция записи информации о пользователе из State_handlers.
+        :param data: State telebot
+        :return:
+        """
+        with self.connection:
+            self.cursor.execute('UPDATE User SET fname=?,lname=?,AGE=? WHERE userID=?',
+                                (data['name'], data['surname'], data['age'], data['id']))
