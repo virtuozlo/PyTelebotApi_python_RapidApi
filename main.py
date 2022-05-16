f33430e6f1e2c3dff0e09ed79bcd073c9aeeb3a2
virtuozlo <iv.chud@yandex.ru>
2022-05-16 09:19:45 +0300

    Добавил custom_filter для отлавливания callback

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from loader import bot
+from keyboards.inline.calendar_inline.filter import bind_filters
 import handlers
 from utils.set_bot_commands import set_default_commands
 
 if __name__ == '__main__':
     set_default_commands(bot)
+    bind_filters(bot)
     bot.polling(none_stop=True)
